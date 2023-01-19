4031bdcbb30da0c5afb854dbc947c18af750f8ac
sujith07cs <123094387+sujith07cs@users.noreply.github.com>
2023-01-19 21:08:27 +0530

    Add files via upload
    This is the Reverse Function of an Array and a String Which can be used to solve in C programming.

---
+++ N1.C
@@ -0,0 +1,35 @@
+// Recursive C program to reverse an array
+#include <stdio.h>
+
+/* Function to reverse arr[] from start to end*/
+void rvereseArray(int arr[], int start, int end)
+{
+int temp;
+if (start >= end)
+	return;
+temp = arr[start];
+arr[start] = arr[end];
+arr[end] = temp;
+rvereseArray(arr, start+1, end-1);
+}	
+
+/* Utility that prints out an array on a line */
+void printArray(int arr[], int size)
+{
+int i;
+for (i=0; i < size; i++)
+	printf("%d ", arr[i]);
+
+printf("\n");
+}
+
+/* Driver function to test above functions */
+int main()
+{
+	int arr[] = {1, 2, 3, 4, 5, 6};
+	printArray(arr, 6);
+	rvereseArray(arr, 0, 5);
+	printf("Reversed array is \n");
+	printArray(arr, 6);
+	return 0;
+}
